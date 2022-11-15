6ef6d35e2befbc5fc528d026ec4aa4b5eed63f23
Ha Nguyen <31268683+earlybird-dev@users.noreply.github.com>
2022-11-15 21:54:54 +1100

    Carousel

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,42 @@
 import React from 'react';
 
+interface Carousel {
+  active: boolean | undefined;
+  src: string;
+  title: string;
+  body: string;
+}
+
+function CarouselItem(props: Carousel) {
+  return (
+    <div className={props.active ? 'carousel-item active' : 'carousel-item'}>
+      <img src={props.src} className="d-block w-100" alt="..."></img>
+      <div className="carousel-caption p-4">
+        <div className="row gx-5">
+          <div className="col-12 col-lg-6">
+            <h3>{props.title}</h3>
+            <p>{props.body}</p>
+          </div>
+          <div className="col-12 col-lg-6 d-flex align-items-center">
+            <div className="row flex-fill">
+              <div className="col">
+                <a className="btn" href="/promos">
+                  See all promos
+                </a>
+              </div>
+              <div className="col ">
+                <a className="btn" href="/signup">
+                  Sign up today
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const Hero = (props: any) => {
   return (
     <section className="stn-hero h-50">
@@ -31,70 +68,25 @@ const Hero = (props: any) => {
           ></button>
         </div>
         <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src="https://pointsbet.com.au/assets/content/au/homecarousel/images/221104-NBA_SGM-CDESK.jpg"
-              className="d-block w-100"
-              alt="..."
-            ></img>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>
-                Some representative placeholder content for the first slide.
-              </p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://pointsbet.com.au/assets/content/au/homecarousel/images/WorldCup-CDESK.jpg"
-              className="d-block w-100"
-              alt="..."
-            ></img>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>
-                Some representative placeholder content for the second slide.
-              </p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://pointsbet.com.au/assets/content/au/homecarousel/images/22MBS-CDESK.png"
-              className="d-block w-100"
-              alt="..."
-            ></img>
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>
-                Some representative placeholder content for the third slide.
-              </p>
-            </div>
-          </div>
+          <CarouselItem
+            active={true}
+            src="https://pointsbet.com.au/assets/content/au/homecarousel/images/WorldCup-CDESK.jpg"
+            title="WORLD CUP - SCORE FIRST YOU WIN"
+            body="Early Payout: On Australia Matches in Group Stage, if your selection scores the first goal of the match we'll pay you out as a winner. First $50 staked."
+          />
+          <CarouselItem
+            active={false}
+            src="https://pointsbet.com.au/assets/content/au/homecarousel/images/221104-NBA_SGM-CDESK.jpg"
+            title="NBA Same Game Multi Offer"
+            body="On your first placed 3+ leg NBA SGM each day this week, if 1 leg fails get your stake refunded in Bonus Bets up to $50."
+          />
+          <CarouselItem
+            active={false}
+            src="https://pointsbet.com.au/assets/content/au/homecarousel/images/22MBS-CDESK.png"
+            title="BONUS BACK TO 4TH EVERY DAY"
+            body="Run 2nd, 3rd or 4th in selected races every day and get Bonus back up to $50."
+          />
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     </section>
   );
