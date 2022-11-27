7b940e6717d576523eb30875234a0ad3b9d313ee
Ha Nguyen <31268683+earlybird-dev@users.noreply.github.com>
2022-11-27 21:10:31 +1100

    Match page hero

--- src/components/MatchHero.tsx
+++ src/components/MatchHero.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PromoIcon } from './SVGIcon';
+import { PromoIcon, VSIcon } from './SVGIcon';
 
 interface Carousel {
   active: boolean | undefined;
@@ -43,56 +43,85 @@ function CarouselItem(props: Carousel) {
 
 const MatchHero = (props: any) => {
   return (
-    <section className="stn-hero">
-      <div
-        id="carouselExampleIndicators"
-        className="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+    <div className="f1jrlq65 f1dhpam1">
+      <section className="stn-match-hero d-flex align-items-center mb-3 mb-md-5 p-3 p-md-5 position-relative">
+        <div className="match-img">
+          <div className=" d-flex flex-column">
+            <div className="w-100 h-100 position-absolute overflow-hidden top-0 start-0">
+              <img
+                className="w-100 h-100"
+                src="https://pointsbet.com.au/assets/img/sports/banners/aussie-rules.banner.md.jpg"
+                alt="aussie-rules"
+              />
+            </div>
+          </div>
         </div>
-        <div className="carousel-inner">
-          <CarouselItem
-            active={true}
-            src="https://pointsbet.com.au/assets/content/au/homecarousel/images/Generic-BTE-CDESK.png"
-            title="BEST TOTE EXOTICS ON EVERY RACE"
-            body="This Saturday on All Races at Kembla Grange & Ballarat."
-          />
-          <CarouselItem
-            active={false}
-            src="https://pointsbet.com.au/assets/content/au/homecarousel/images/WorldCup-CDESK.jpg"
-            title="WORLD CUP - SCORE FIRST YOU WIN"
-            body="Early Payout: On Australia Matches in Group Stage, if your selection scores the first goal of the match we'll pay you out as a winner. First $50 staked."
-          />
-          <CarouselItem
-            active={false}
-            src="https://pointsbet.com.au/assets/content/au/homecarousel/images/22MBS-CDESK.png"
-            title="BONUS BACK TO 4TH EVERY DAY"
-            body="Run 2nd, 3rd or 4th in selected races every day and get Bonus back up to $50."
-          />
+        <div className="match-info d-block my-0 mx-auto w-100 position-relative">
+          <div className=" d-flex flex-column">
+            <div className="d-flex flex-column justify-content-center align-items-center text-white gap-4">
+              <div className="f1w6cbtc">
+                <div className="f9wge8m d-flex flex-column justify-content-center align-items-center position-relative px-5">
+                  <div className="match-time">2:40PM</div>
+                  <div className="match-day">
+                    <span>Today</span>
+                  </div>
+                  <div className="d-inline-block position-absolute end-0">
+                    <button className="d-flex border-0 align-items-center justify-content-center rounded-circle">
+                      <span className="f92nxf7 f1ry9gvn">
+                        <svg
+                          width="12"
+                          height="12"
+                          className="fpz9ar8"
+                          viewBox="0 -64 1024 1024"
+                          xmlns="http://www.w3.org/2000/svg"
+                          strokeWidth="1"
+                        >
+                          <path
+                            transform="translate(0, 896), scale(1, -1)"
+                            stroke="#ED1B42"
+                            fill="#FFF"
+                            fillOpacity="0.5"
+                            d="M512 115.2l-486.4 486.4 119.467 119.467 366.933-366.933 366.933 366.933 119.467-119.467z"
+                          ></path>
+                        </svg>
+                      </span>
+                    </button>
+                    <div className="f1fqtcfr"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="f96or0t w-100">
+                <div className="f96or0t w-100">
+                  <span></span>
+                  <div className="f96or0t w-100">
+                    <h2 className="d-flex align-items-center justify-content-center w-100">
+                      <span className="match-team d-flex justify-content-end align-items-center">
+                        Brisbane Lions Women
+                      </span>
+                      <span className="f1m8uqsy">
+                        <span className="fnlae7i f1ry9gvn mx-2 mx-md-3">
+                          <VSIcon />
+                        </span>
+                      </span>
+                      <span className="match-team d-flex justify-content-start align-items-center">
+                        Melbourne Women
+                      </span>
+                    </h2>
+                  </div>
+                  <span></span>
+                </div>
+              </div>
+              <span className="fvyrjs fwa3sdj f1908qkc">3h 13m</span>
+              <div className="f11z1geh f1mxy1xb">
+                <span className=""></span>
+                <span className=""></span>
+                <span className=""></span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
